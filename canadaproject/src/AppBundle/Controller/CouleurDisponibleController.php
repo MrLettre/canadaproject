617cb8cbb2f02cb89b32b32b7bc1c6c0cddbd736
Unknown <max.garnier@live.fr>
2018-09-17 16:09:23 +0200

    modification BDD-> ajout gestion couleurs avant vichuploder // correction entité pays

---
+++ canadaproject/src/AppBundle/Controller/CouleurDisponibleController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\CouleurDisponible;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Couleurdisponible controller.
+ *
+ * @Route("couleurdisponible")
+ */
+class CouleurDisponibleController extends Controller
+{
+    /**
+     * Lists all couleurDisponible entities.
+     *
+     * @Route("/", name="couleurdisponible_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $couleurDisponibles = $em->getRepository('AppBundle:CouleurDisponible')->findAll();
+
+        return $this->render('couleurdisponible/index.html.twig', array(
+            'couleurDisponibles' => $couleurDisponibles,
+        ));
+    }
+
+    /**
+     * Creates a new couleurDisponible entity.
+     *
+     * @Route("/new", name="couleurdisponible_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $couleurDisponible = new Couleurdisponible();
+        $form = $this->createForm('AppBundle\Form\CouleurDisponibleType', $couleurDisponible);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($couleurDisponible);
+            $em->flush();
+
+            return $this->redirectToRoute('couleurdisponible_show', array('id' => $couleurDisponible->getId()));
+        }
+
+        return $this->render('couleurdisponible/new.html.twig', array(
+            'couleurDisponible' => $couleurDisponible,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a couleurDisponible entity.
+     *
+     * @Route("/{id}", name="couleurdisponible_show")
+     * @Method("GET")
+     */
+    public function showAction(CouleurDisponible $couleurDisponible)
+    {
+        $deleteForm = $this->createDeleteForm($couleurDisponible);
+
+        return $this->render('couleurdisponible/show.html.twig', array(
+            'couleurDisponible' => $couleurDisponible,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing couleurDisponible entity.
+     *
+     * @Route("/{id}/edit", name="couleurdisponible_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, CouleurDisponible $couleurDisponible)
+    {
+        $deleteForm = $this->createDeleteForm($couleurDisponible);
+        $editForm = $this->createForm('AppBundle\Form\CouleurDisponibleType', $couleurDisponible);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('couleurdisponible_edit', array('id' => $couleurDisponible->getId()));
+        }
+
+        return $this->render('couleurdisponible/edit.html.twig', array(
+            'couleurDisponible' => $couleurDisponible,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a couleurDisponible entity.
+     *
+     * @Route("/{id}", name="couleurdisponible_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, CouleurDisponible $couleurDisponible)
+    {
+        $form = $this->createDeleteForm($couleurDisponible);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($couleurDisponible);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('couleurdisponible_index');
+    }
+
+    /**
+     * Creates a form to delete a couleurDisponible entity.
+     *
+     * @param CouleurDisponible $couleurDisponible The couleurDisponible entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(CouleurDisponible $couleurDisponible)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('couleurdisponible_delete', array('id' => $couleurDisponible->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ canadaproject/src/AppBundle/Form/CouleurDisponibleType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CouleurDisponibleType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('nom')->add('version');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\CouleurDisponible'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_couleurdisponible';
+    }
+
+
+}
