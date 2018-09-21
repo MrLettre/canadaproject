6666e3646b9c3374e974b93cbb92367fae0d0d7c
Kyle Brace <kylebrace1@gmail.com>
2018-09-21 10:37:11 +0200

    Exemple rustique de l'UI de l'admin du site

--- canadaproject/src/AppBundle/Controller/AdminController.php
+++ canadaproject/src/AppBundle/Controller/AdminController.php
@@ -25,4 +25,54 @@ class AdminController extends Controller
         // replace this example code with whatever you need
         return $this->render('admin/admin/indexSeller.html.twig');
     }
+
+
+    //Route pour les actions de la page Admin du site 
+
+
+    /**
+     * @Route("/adminMySeller", name="adminMySeller")
+     */
+    public function adminMySeller()
+    {
+        // replace this example code with whatever you need
+        return $this->render('admin/admin/adminMySeller.html.twig');
+    }
+
+    /**
+     * @Route("/adminMyClient", name="adminMyClient")
+     */
+    public function adminMyClient()
+    {
+        // replace this example code with whatever you need
+        return $this->render('admin/admin/adminMyClient.html.twig');
+    }
+
+    /**
+     * @Route("/adminStats", name="adminStats")
+     */
+    public function adminStats()
+    {
+        // replace this example code with whatever you need
+        return $this->render('admin/admin/adminStats.html.twig');
+    }
+
+    /**
+     * @Route("/adminAfterSale", name="adminAfterSale")
+     */
+    public function adminAfterSale()
+    {
+        // replace this example code with whatever you need
+        return $this->render('admin/admin/adminAfterSale.html.twig');
+    }
+
+     /**
+     * @Route("/adminAutoPark", name="adminAutoParc")
+     */
+    public function adminAutoParc()
+    {
+        // replace this example code with whatever you need
+        return $this->render('admin/admin/adminAutoPark.html.twig');
+    }
+
 }
