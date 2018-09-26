076f4d85a22d3f87a36e3b75b4051ddabbe703e8
Unknown <max.garnier@live.fr>
2018-09-26 16:52:05 +0200

    ajax presque bon

---
+++ canadaproject/assets/js/rechercheVehiculeVendeur.js
@@ -0,0 +1,17 @@
+$(document).on('change', '#appbundle_vehiculephysique_marque, #appbundle_vehiculephysique_model', function () {
+    let $field = $(this)
+    let $marqueField = $('#appbundle_vehiculephysique_marque')
+    let $form = $field.closest('form')
+    let target = '#' + $field.attr('id').replace('model', 'version').replace('marque', 'model')
+    // Les données à envoyer en Ajax
+    let data = {}
+    data[$marqueField.attr('name')] = $marqueField.val()
+    data[$field.attr('name')] = $field.val()
+    // On soumet les données
+    $.post($form.attr('action'), data).then(function (data) {
+        // On récupère le nouveau <select>
+        let $input = $(data).find(target)
+        // On remplace notre <select> actuel
+        $(target).replaceWith($input)
+    })
+})
